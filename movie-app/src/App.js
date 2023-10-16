09a800894b66d2dd83eac6c43ad81eb943e36e25
kimbela winston <kimbelawin84@gmail.com>
2023-10-15 22:21:43 -0500

    moved useEffect to App component

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MoviesList from './MoviesList';
 import AddMovie from './AddMovie';
@@ -6,8 +6,26 @@ import NavBar from './NavBar';
 import './style.css';
 
 function App() {
+  // const [movies, setMovies] = useState([]);
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((response) => {
+        if (!response.ok) throw new Error('Network response was not ok');
+        return response.json();
+      })
+      .then((data) => setMovies(data))
+      .catch(error => {
+        console.error("There was a problem with the fetch operation:", error.message);
+        setError(error.message);
+      });
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
   const addMovieToList = (newMovie) => {
     setMovies([...movies, newMovie]);
   };
--- movie-app/src/MoviesList.js
+++ movie-app/src/MoviesList.js
@@ -1,26 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './style.css';
 
-function MoviesList() {
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/movies")
-      .then((response) => {
-        if (!response.ok) throw new Error('Network response was not ok');
-        return response.json();
-      })
-      .then((data) => setMovies(data))
-      .catch(error => {
-        console.error("There was a problem with the fetch operation:", error.message);
-        setError(error.message);
-      });
-  }, []);
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+function MoviesList({movies}) {
+  
 
   return (
     <div className="container">
